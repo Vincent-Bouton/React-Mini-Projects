1ce847b71d617412e5ab536f7eff85ed2764bc3a
Vincent Bouton <vincent.bouton.dev@gmail.com>
2020-02-24 16:48:38 +0100

    Get the new deck & add UUID library

--- cards-api/src/Cards.jsx
+++ cards-api/src/Cards.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 import "./Cards.css";
+import axios from "axios";
+import Card from "./Card";
 
 class Cards extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+  componentDidMount() {
+    let newDeck;
+    axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/").then(deck => {
+      newDeck = deck;
+      console.log(newDeck);
+    });
+  }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Card />
+      </div>
+    );
   }
 }
 
