76e60c0d3e2743625416b3186178a164d0e13844
Vincent Bouton <vincent.bouton.dev@gmail.com>
2020-02-24 16:30:47 +0100

    Add Card component

---
+++ cards-api/src/Card.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Card.css";
+
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Card;
