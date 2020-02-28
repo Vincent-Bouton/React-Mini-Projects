531ffb8ed3e094913c98af7056dfa151db1d8f85
Vincent Bouton <vincent.bouton.dev@gmail.com>
2020-02-28 16:07:26 +0100

    add NavLink

--- react-rooter-test/src/App.js
+++ react-rooter-test/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch, NavLink } from "react-router-dom";
 import About from "./About";
 import Contact from "./Contact";
 import Home from "./Home";
@@ -9,9 +9,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Route exact path="/contact" component={Contact} />
-      <Route exact path="/About" component={About} />
-      <Route exact path="/" component={Home} />
+      <nav>
+        <NavLink exact activeClassName={"active-link"} to="/contact">
+          Contact
+        </NavLink>
+        <NavLink exact activeClassName={"active-link"} to="/about">
+          About
+        </NavLink>
+        <NavLink exact activeClassName={"active-link"} to="/">
+          Home
+        </NavLink>
+      </nav>
+      <Switch>
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/About" component={About} />
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
